90c6a6aae923b863e110a6af9cbdbf58136d808c
Chidotozuki <olatunjiolanrewaju228@gmail.com>
2025-09-05 00:59:25 +0100

    style: standardize import statements and formatting in authRoutes.js and server.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import {login, signup, updateProfile, changePassword } from '../controllers/authController.js';
-import { authenticate } from '../middleware/authMiddleware.js';
+import express from "express";
+import {
+  login,
+  signup,
+  updateProfile,
+  changePassword,
+} from "../controllers/authController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/login', login);
-router.post('/signup', signup);
-router.put('/profile', authenticate, updateProfile);
-router.put('/change-password', authenticate, changePassword);
+router.post("/login", login);
+router.post("/signup", signup);
+router.put("/profile", authenticate, updateProfile);
+router.put("/change-password", authenticate, changePassword);
 
 // later
 // router.post('/request-reset', requestPasswordReset);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js'
-import noteRoutes from './routes/noteRoutes.js';
-import { connectDB } from './config/database.js';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import noteRoutes from "./routes/noteRoutes.js";
+import { connectDB } from "./config/database.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from 'cors';
+import cors from "cors";
 import path from "path";
 
 dotenv.config();
@@ -23,23 +23,24 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 app.use("/api/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 }
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database", error);
+    process.exit(1);
   });
-}).catch((error) => {
-  console.error("Failed to connect to the database", error);
-  process.exit(1);
-});
-
